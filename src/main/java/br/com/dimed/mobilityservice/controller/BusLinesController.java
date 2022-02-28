98ef372c64645c9c1f6cadda288dac7a59ba0728
Monise <monise.volinger@gmail.com>
2022-02-28 19:28:09 -0300

    Adding Swagger configurations.

--- src/main/java/br/com/dimed/mobilityservice/controller/BusLinesController.java
+++ src/main/java/br/com/dimed/mobilityservice/controller/BusLinesController.java
@@ -3,6 +3,9 @@ package br.com.dimed.mobilityservice.controller;
 import br.com.dimed.mobilityservice.dto.BusLineDTO;
 import br.com.dimed.mobilityservice.service.BusLineService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +22,10 @@ public class BusLinesController {
 
     public static final String BASE_URL = "/api/bus/lines";
 
+    @ApiOperation(value = "Get information about bus lines by name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Information about bus lines gotten successfully")
+    })
     @GetMapping(
             value = "/{name}",
             produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/br/com/dimed/mobilityservice/controller/BusRoutesController.java
+++ src/main/java/br/com/dimed/mobilityservice/controller/BusRoutesController.java
@@ -3,6 +3,9 @@ package br.com.dimed.mobilityservice.controller;
 import br.com.dimed.mobilityservice.dto.BusRouteDTO;
 import br.com.dimed.mobilityservice.service.BusRouteService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,6 +20,10 @@ public class BusRoutesController {
 
     public static final String BASE_URL = "/api/bus/routes";
 
+    @ApiOperation(value = "Get information about bus routes by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Information about bus routes gotten successfully")
+    })
     @GetMapping(
             value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
